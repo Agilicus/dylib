6f0fb5cee3102da87b0cd28f50c1ac0cfefaab84
Kyle Larose <kyle@agilicus.com>
2021-10-20 21:02:04 -0400

    remove all MySyscall bits
    Forgot a few in the last commit

--- dylib_windows.go
+++ dylib_windows.go
@@ -1,8 +1,6 @@
 package dylib
 
 import (
-	"fmt"
-	"strconv"
 	"syscall"
 )
 
@@ -39,50 +37,7 @@ func (d *LazyDLL) Close() {
 }
 
 func (d *LazyDLL) call(proc *LazyProc, a ...uintptr) (r1, r2 uintptr, lastErr error) {
-	// 没到找到我封装的那个系统函数，就使用原始的
-	if d.mySyscall == nil {
-		return proc.CallOriginal(a...)
-	}
-	err := proc.Find()
-	if err != nil {
-		fmt.Println("proc \"" + proc.lzProc.Name + "\" not find.")
-		return 0, 0, syscall.EINVAL
-	}
-	addr := proc.Addr()
-	if addr != 0 {
-		pLen := uintptr(len(a))
-		switch pLen {
-		case 0:
-			return d.mySyscall.Call(addr, pLen)
-		case 1:
-			return d.mySyscall.Call(addr, pLen, a[0])
-		case 2:
-			return d.mySyscall.Call(addr, pLen, a[0], a[1])
-		case 3:
-			return d.mySyscall.Call(addr, pLen, a[0], a[1], a[2])
-		case 4:
-			return d.mySyscall.Call(addr, pLen, a[0], a[1], a[2], a[3])
-		case 5:
-			return d.mySyscall.Call(addr, pLen, a[0], a[1], a[2], a[3], a[4])
-		case 6:
-			return d.mySyscall.Call(addr, pLen, a[0], a[1], a[2], a[3], a[4], a[5])
-		case 7:
-			return d.mySyscall.Call(addr, pLen, a[0], a[1], a[2], a[3], a[4], a[5], a[6])
-		case 8:
-			return d.mySyscall.Call(addr, pLen, a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7])
-		case 9:
-			return d.mySyscall.Call(addr, pLen, a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7], a[8])
-		case 10:
-			return d.mySyscall.Call(addr, pLen, a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7], a[8], a[9])
-		case 11:
-			return d.mySyscall.Call(addr, pLen, a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7], a[8], a[9], a[10])
-		case 12:
-			return d.mySyscall.Call(addr, pLen, a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7], a[8], a[9], a[10], a[11])
-		default:
-			panic("Call " + proc.lzProc.Name + " with too many arguments " + strconv.Itoa(len(a)) + ".")
-		}
-	}
-	return 0, 0, syscall.EINVAL
+	return proc.CallOriginal(a...)
 }
 
 func (p *LazyProc) Addr() uintptr {
